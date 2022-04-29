a21edbcff106755e18db92637206212b3dce67a3
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-29 12:23:48 +0200

    refactor(store,selection)!: Remove selectedNodesBbox from store
    # What's changed?
    
    * Remove selectedNodesBbox from store state
    * Will be handled internally by NodesSelection component

--- package/src/types/store.ts
+++ package/src/types/store.ts
@@ -48,7 +48,6 @@ export interface State extends Omit<FlowOptions, 'id' | 'modelValue'> {
   onlyRenderVisibleElements: boolean
   defaultPosition: [number, number]
 
-  selectedNodesBbox: Rect
   nodesSelectionActive: boolean
   userSelectionActive: boolean
   multiSelectionActive: boolean
